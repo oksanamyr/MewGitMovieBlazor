798aef152f16fd499ddc2dba253e8fbdcf7fa7bb
oksanamyr <Oksana.Myronovych@ndsu.edu>
2024-11-19 20:23:26 -0600

    Movies DB and Links

---
+++ Data/MovieAppForGitContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieAppForGit.Models;
+
+namespace MovieAppForGit.Data
+{
+    public class MovieAppForGitContext : DbContext
+    {
+        public MovieAppForGitContext (DbContextOptions<MovieAppForGitContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieAppForGit.Models.Movie> Movie { get; set; } = default!;
+    }
+}
